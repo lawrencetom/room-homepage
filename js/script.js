de5cba4a4557dbb08b146f3cd044e1ecc8020627
Tom Lawrence <tom.markus.lawrence@gmail.com>
2021-02-14 12:56:44 +0100

    mobile complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const header = document.querySelector("#header")
+const btnHamburger = document.querySelector("#btnHamburger")
+const arrowLeft = document.querySelector("#arrow-left")
+const arrowRight = document.querySelector("#arrow-right")
 
-header.addEventListener("click", function() {
+var i = 0
+var mobileHeroImages = []
+mobileHeroImages[0] = document.querySelector("#img-hero-1") 
+mobileHeroImages[1] = document.querySelector("#img-hero-2") 
+mobileHeroImages[2] = document.querySelector("#img-hero-3") 
+
+var heroTexts = []
+heroTexts[0] = document.querySelector("#hero-1")
+heroTexts[1] = document.querySelector("#hero-2")
+heroTexts[2] = document.querySelector("#hero-3")
+
+btnHamburger.addEventListener("click", function() {
     console.log("click Hamburger")
 
     if (header.classList.contains("open")) {
@@ -9,4 +23,39 @@ header.addEventListener("click", function() {
     else {
         header.classList.add("open")
     }
+})
+
+arrowLeft.addEventListener("click", function() {
+    console.log("click Arrow Left")
+
+    mobileHeroImages[i].classList.remove("visible-img")
+    heroTexts[i].classList.remove("visible-text")
+
+    if (i > 0) {
+        i--
+    } 
+    else {
+        i = mobileHeroImages.length - 1
+    }
+
+    mobileHeroImages[i].classList.add("visible-img")
+    heroTexts[i].classList.add("visible-text")
+
+})
+
+arrowRight.addEventListener("click", function() {
+    console.log("click Arrow Right")
+
+    mobileHeroImages[i].classList.remove("visible-img")
+    heroTexts[i].classList.remove("visible-text")
+
+    if (i < (mobileHeroImages.length - 1)) {
+        i++
+    } 
+    else {
+        i = 0
+    }
+    mobileHeroImages[i].classList.add("visible-img")
+    heroTexts[i].classList.add("visible-text")
+
 })
