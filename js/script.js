2ac7fd65f4e7c1ef35de31f78f49f5cee16fabfc
Tom Lawrence <tom.markus.lawrence@gmail.com>
2021-02-15 15:27:13 +0100

    final

--- js/script.js
+++ js/script.js
@@ -2,12 +2,29 @@ const header = document.querySelector("#header")
 const btnHamburger = document.querySelector("#btnHamburger")
 const arrowLeft = document.querySelector("#arrow-left")
 const arrowRight = document.querySelector("#arrow-right")
+const mediaQuery = window.matchMedia('(min-width: 1100px)')
+
+function autoCloseNavMenu(w) {
+    if (w.matches) { // If media query matches
+        if (header.classList.contains("open")) {
+            header.classList.remove("open")
+        }
+    } 
+}
+
+autoCloseNavMenu(mediaQuery)
+mediaQuery.addListener(autoCloseNavMenu)
 
 var i = 0
 var mobileHeroImages = []
-mobileHeroImages[0] = document.querySelector("#img-hero-1") 
-mobileHeroImages[1] = document.querySelector("#img-hero-2") 
-mobileHeroImages[2] = document.querySelector("#img-hero-3") 
+mobileHeroImages[0] = document.querySelector("#mobile-img-hero-1") 
+mobileHeroImages[1] = document.querySelector("#mobile-img-hero-2") 
+mobileHeroImages[2] = document.querySelector("#mobile-img-hero-3") 
+
+var desktopHeroImages = []
+desktopHeroImages[0] = document.querySelector("#desktop-img-hero-1")
+desktopHeroImages[1] = document.querySelector("#desktop-img-hero-2")
+desktopHeroImages[2] = document.querySelector("#desktop-img-hero-3")
 
 var heroTexts = []
 heroTexts[0] = document.querySelector("#hero-1")
@@ -29,6 +46,7 @@ arrowLeft.addEventListener("click", function() {
     console.log("click Arrow Left")
 
     mobileHeroImages[i].classList.remove("visible-img")
+    desktopHeroImages[i].classList.remove("visible-img")
     heroTexts[i].classList.remove("visible-text")
 
     if (i > 0) {
@@ -39,6 +57,7 @@ arrowLeft.addEventListener("click", function() {
     }
 
     mobileHeroImages[i].classList.add("visible-img")
+    desktopHeroImages[i].classList.add("visible-img")
     heroTexts[i].classList.add("visible-text")
 
 })
@@ -47,6 +66,7 @@ arrowRight.addEventListener("click", function() {
     console.log("click Arrow Right")
 
     mobileHeroImages[i].classList.remove("visible-img")
+    desktopHeroImages[i].classList.remove("visible-img")
     heroTexts[i].classList.remove("visible-text")
 
     if (i < (mobileHeroImages.length - 1)) {
@@ -56,6 +76,7 @@ arrowRight.addEventListener("click", function() {
         i = 0
     }
     mobileHeroImages[i].classList.add("visible-img")
+    desktopHeroImages[i].classList.add("visible-img")
     heroTexts[i].classList.add("visible-text")
 
 })
